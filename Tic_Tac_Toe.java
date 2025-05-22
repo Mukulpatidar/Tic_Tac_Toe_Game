beb0f5f30775bc7055b10fcb8fe1419e8ee5f8fe
Mukulpatidar <patidarmukul127.com>
2025-05-22 07:01:54 +0530

    Create Frame

--- Tic_Tac_Toe.java
+++ Tic_Tac_Toe.java
@@ -1,5 +1,19 @@
+import java.awt.*;
+import java.awt.event.*;
+class FDemo extends Frame{
+	
+	FDemo(){
+		
+	}
+	
+}
+
 class Tic_Tac_Toe{
 	public static void main(String ar[]){
-		System.out.print("hello");
+		FDemo f=new FDemo();
+		f.setVisible(true);
+		f.setLocation(200,200);
+		f.setSize(500,600);
+		// f.setDefaultCloseOperation(f.EXIT_ON_CLOSE);
 	}
 }
