7fb5bd0ee7d08b2d2525d0b8a0dd503e31900143
Mukulpatidar <patidarmukul127.com>
2025-05-22 22:55:38 +0530

    Add button for cliking on 0 and X

--- Tic_Tac_Toe.java
+++ Tic_Tac_Toe.java
@@ -1,9 +1,26 @@
 import java.awt.*;
 import java.awt.event.*;
 class FDemo extends Frame{
-	
+	Button b1[]=new Button[9];
+	int x=100,y=100;
+	int c=1;
 	FDemo(){
-		
+		Font f=new Font(" ",Font.ITALIC,30);
+		setFont(f);
+		setLayout(null);
+		int k=0;
+		for(int i=1;i<=3;i++){
+			for(int j=1;j<=3;j++){
+				b1[k]=new Button();
+				b1[k].setSize(100,100);
+				b1[k].setLocation(x,y);
+				add(b1[k]);
+				k++;
+				x+=100;
+			}
+			y+=100;
+			x=100;
+		}
 	}
 	
 }
