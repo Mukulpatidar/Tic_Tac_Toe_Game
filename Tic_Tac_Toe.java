e272f51354b4869253f9fd87a8dcc9c52299bcc1
Mukulpatidar <patidarmukul127.com>
2025-06-19 09:30:45 +0530

    change background color and button with font & color

--- Tic_Tac_Toe.java
+++ Tic_Tac_Toe.java
@@ -4,18 +4,22 @@ class FDemo extends Frame implements ActionListener{
 	Button b1[]=new Button[9];
 	Button restart;
 	int turn=1;
-	int x=70,y=100;
+	int x=40,y=60;
 	int c=1;
 	FDemo(){
 		Font f=new Font(" ",Font.ITALIC,30);
 		
 		setFont(f);
 		setLayout(null);
+		  setBackground(new Color(240, 248, 255)); 
 		
 		int k=0;
 		for(int i=1;i<=3;i++){
 			for(int j=1;j<=3;j++){
 				b1[k]=new Button();
+				 b1[k].setFont(new Font("Arial", Font.BOLD, 30));
+                b1[k].setBackground(Color.WHITE);
+                b1[k].setForeground(Color.DARK_GRAY);
 				b1[k].setSize(100,100);
 				b1[k].setLocation(x,y);
 				add(b1[k]);
@@ -24,7 +28,7 @@ class FDemo extends Frame implements ActionListener{
 				x+=100;
 			}
 			y+=100;
-			x=70;
+			x=40;
 		}
 		
 		addWindowListener(new WindowAdapter() {
@@ -36,7 +40,7 @@ class FDemo extends Frame implements ActionListener{
 	
 	 restart = new Button("Restart");
         restart.setFont(new Font("Arial", Font.BOLD, 20));
-        restart.setBounds(120, 420, 200, 50);
+        restart.setBounds(100, 370, 200, 50);
 		restart.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                resetGame();
@@ -141,7 +145,7 @@ class Tic_Tac_Toe{
 		FDemo f=new FDemo();
 		f.setVisible(true);
 		f.setLocation(200,200);
-		f.setSize(420,480);
+		f.setSize(380,430);
 		f.setResizable(false);
 		f.setTitle("Tic Tac Toe - AWT Game");
 		
